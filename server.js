2cada4fe25a60837a0fbd14d8b7edc9351828692
Alexandr Abrosimov <alex.a@mactelabs.com>
2021-11-21 21:53:03 +0700

    clean files of project

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use('*', function (req, res) {
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`)
 })
