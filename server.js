6a3561667f88d996b7dcc797f03db3d50511d1f1
Alexandr Abrosimov <alex.a@mactelabs.com>
2021-11-23 21:33:20 +0700

    Changed server.js PORT

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use('*', function (req, res) {
 });
 
 
-const port = 3000;
+const PORT = process.env.PORT || 3000; 
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
 })
